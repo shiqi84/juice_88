423d21943cc9285633bfa1a24adec9dc44a6af7e
shiqi84 <shiqi.wan@stonybrook.edu>
2021-10-22 17:48:38 -0400

    mine

---
+++ client/build/precache-manifest.bcabb48cf4e4de44e5545170a12a08aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f7c478af5e1e903f61872526db4d38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15b66eccb7d1467e9ef3",
+    "url": "/static/css/main.5580c6cd.chunk.css"
+  },
+  {
+    "revision": "06a1545ef7462adf9eab",
+    "url": "/static/js/2.20a6a03f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.20a6a03f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b66eccb7d1467e9ef3",
+    "url": "/static/js/main.bfe80ae4.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  }
+]);
